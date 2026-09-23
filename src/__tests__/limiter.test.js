5609390b8cf33eaab78de98c103e1510234ad00b
KirillMatviychuck <kirill.matviychuck@gmail.com>
2026-09-23 20:21:39 +0300

    refactor: update limiter tests for MongoDB

--- src/__tests__/limiter.test.js
+++ src/__tests__/limiter.test.js
@@ -1,37 +1,27 @@
 const request = require('supertest');
 const app = require('../app/app');
+const UserModel = require('../Schemas/User')
 
-
-jest.mock('../config/db', () => ({
-    getDB: jest.fn().mockResolvedValue({
-        data: {
-            movies: [
-                { id: 1, title: 'Inception', year: 2010 },
-                { id: 2, title: 'The Matrix', year: 1999 },
-            ],
-            users: [
-                { id: 1, email: 'admin@test.com', password: '$2b$10$.OLBgajGECqyaTuEwlywx.e1ltDdk/AlVLrjANYofvm9naIrPQH7q' }
-            ],
-            refreshTokens: [],
-        },
-        write: jest.fn().mockResolvedValue(undefined),
-    }),
-}))
+jest.mock('../Schemas/User')
 
 describe('check auth limiter for login', () => {
     it('over 5 request in 15 minutes', async () => {
-
+        UserModel.findOne.mockResolvedValue({
+            _id: '1',
+            email: 'admin@test.com',
+            password: '555557'
+        })
         for (let i = 0; i < 5; i++) {
             const response = await request(app)
                 .post('/auth/login')
-                .send({ email: 'jonny@gmail.com', password: '555557' })
+                .send({ email: 'admin@test.com', password: '555557' })
 
             expect(response.status).not.toBe(429)
         }
 
         const response = await request(app)
             .post('/auth/login')
-            .send({ email: 'jonny@gmail.com', password: '555557' })
+            .send({ email: 'admin@test.com', password: '555557' })
 
         expect(response.status).toBe(429)
     })
